7667a250d828f9eea46640ed237b24c82322317b
Utsav Jariwala <utsav.jariwala@zeuslearning.com>
2025-09-11 18:59:27 +0530

    Implement logging in hello_world_view to create and read log entries

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def hello_world_view(request):
+    file_path = "/tmp/logs"
+    # Create the file and write to it
+    with open(file_path, "w") as f:
+        f.write("This is the first log entry\n")
+    with open(file_path, "r") as f:
+        content = f.read()
+        print("Content of logs file inside container:")
+        print(content)
     return HttpResponse("Hello World")
