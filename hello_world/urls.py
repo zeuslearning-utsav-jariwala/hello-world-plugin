6ac471a2f7502be91124a77992aea93cfd045ca6
Utsav Jariwala <utsav.jariwala@zeuslearning.com>
2025-09-11 20:31:25 +0530

    abc

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,10 +1,10 @@
 """
 URLs for hello_world.
 """
-from django.urls import re_path  # pylint: disable=unused-import
+from django.urls import path  # pylint: disable=unused-import
 from django.views.generic import TemplateView  # pylint: disable=unused-import
 from .views import hello_world_view
 
 urlpatterns = [
-    re_path(r'^hello/$', hello_world_view, name='hello_world'),
+    path('hello/', hello_world_view, name='hello_world'),
 ]
